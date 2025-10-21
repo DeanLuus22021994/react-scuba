4ce0dffffea650839dc5a78569d0e67ff380e2b4
DeanLuus22021994 <deanluus22021994@gmail.com>
2025-10-21 19:10:08 +0000

    chore: remove markdown extensions from devcontainer.json

--- src/data/oceanSpirit.js
+++ src/data/oceanSpirit.js
@@ -4,27 +4,31 @@
 export const oceanSpiritContent = {
   name: 'Ocean Spirit Scuba Diving Mauritius',
   tagline: 'PADI 5 Star ECO Green Fins Scuba Diving Centre',
-  
+
   contact: {
     whatsapp: '+230 5255 2732',
     office: '+230 2634468',
     email: 'info@osdiving.com',
     location: 'Coastal Road Pereybere Mauritius',
-    googleMapsUrl: 'https://goo.gl/maps/TQAZLNVg7Jk9MtfG7'
+    googleMapsUrl: 'https://goo.gl/maps/TQAZLNVg7Jk9MtfG7',
   },
 
   about: {
-    description: 'Scuba diving in Mauritius means that from the day you arrive, you are among friends. Ocean Spirit Scuba Diving Mauritius is a friendly sociable PADI 5 Star ECO Green Fins Scuba Diving centre with a Green Star, and comfortable club like facilities.',
-    instructors: 'Our patient skilled PADI Instructors focus on safety, they know their fish and love their world.',
-    diving: 'Diving Mauritius is in warm water, with good visibility and 65 great dive sites around the Northern Mauritius Islands. Wreck diving, Turtle diving, Shark diving, macro photography, deep diving, and exquisite healthy coral reefs.',
-    values: 'Ocean Spirit Scuba Diving has grown in its commitment to promote the core principles of conservation, humility, respect, family values and career development.'
+    description:
+      'Scuba diving in Mauritius means that from the day you arrive, you are among friends. Ocean Spirit Scuba Diving Mauritius is a friendly sociable PADI 5 Star ECO Green Fins Scuba Diving centre with a Green Star, and comfortable club like facilities.',
+    instructors:
+      'Our patient skilled PADI Instructors focus on safety, they know their fish and love their world.',
+    diving:
+      'Diving Mauritius is in warm water, with good visibility and 65 great dive sites around the Northern Mauritius Islands. Wreck diving, Turtle diving, Shark diving, macro photography, deep diving, and exquisite healthy coral reefs.',
+    values:
+      'Ocean Spirit Scuba Diving has grown in its commitment to promote the core principles of conservation, humility, respect, family values and career development.',
   },
 
   certifications: [
     'PADI 5 Star ECO Centre',
     'Green Fins Member',
     'Reef-World Federation Partner',
-    'Green Star Rating'
+    'Green Star Rating',
   ],
 
   specialties: [
@@ -34,35 +38,35 @@ export const oceanSpiritContent = {
     'Macro Photography',
     'Deep Diving',
     'Coral Reef Exploration',
-    'Open Water Shark Diving (45 min boat ride to Mascarene Plateau)'
+    'Open Water Shark Diving (45 min boat ride to Mascarene Plateau)',
   ],
 
   temperature: {
     summer: {
       air: {
         range: '28°C - 35°C',
-        months: 'November - May'
+        months: 'November - May',
       },
       water: {
-        range: '26°C - 29°C'
-      }
+        range: '26°C - 29°C',
+      },
     },
     winter: {
       air: {
         range: '18°C - 28°C',
-        months: 'April - October'
+        months: 'April - October',
       },
       water: {
-        range: '23°C - 25°C'
-      }
-    }
+        range: '23°C - 25°C',
+      },
+    },
   },
 
   offers: [
     {
       name: 'Discover Scuba Diving',
       description: 'PADI and Ocean Spirit - Step one of your PADI Open Water Certification',
-      type: 'Couples DSD Special'
+      type: 'Couples DSD Special',
     },
     {
       name: 'Best of the North - 10 Dive Package',
@@ -70,9 +74,9 @@ export const oceanSpiritContent = {
       includes: [
         '10 dives total',
         '2 dive Safari to Coin de Mire island',
-        'Trou Aux Biches diving'
-      ]
-    }
+        'Trou Aux Biches diving',
+      ],
+    },
   ],
 
   diveSites: [
@@ -81,7 +85,7 @@ export const oceanSpiritContent = {
     'Djabeda Ship Wreck',
     'Shallow Coral Reef',
     'Whale Rock',
-    'Deep sites around Northern Mauritius'
+    'Deep sites around Northern Mauritius',
   ],
 
   social: {
@@ -89,14 +93,15 @@ export const oceanSpiritContent = {
     instagram: 'https://www.instagram.com/divingmauritius/',
     twitter: 'https://twitter.com/OceanSpirit13',
     youtube: 'https://www.youtube.com/user/oceandivermu',
-    tripAdvisor: 'https://www.tripadvisor.com.my/Attraction_Review-g1905319-d1717410-Reviews-Ocean_Spirit_Dive_Center-Pereybere.html'
+    tripAdvisor:
+      'https://www.tripadvisor.com.my/Attraction_Review-g1905319-d1717410-Reviews-Ocean_Spirit_Dive_Center-Pereybere.html',
   },
 
   images: {
     logo: '/photos/logo-ocean-spirit.png',
     hero: '/photos/hero-ocean-spirit.jpg',
-    banner: '/photos/banner-underwater.jpg'
-  }
+    banner: '/photos/banner-underwater.jpg',
+  },
 };
 
 export default oceanSpiritContent;
